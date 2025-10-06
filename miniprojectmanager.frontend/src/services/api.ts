39b2b731d3e4c1c1b90bca0db5851298e356adac
DanielTzafrir <74878286+DanielTzafrir@users.noreply.github.com>
2025-10-06 08:16:52 +0300

    Deployment front to render part 2

--- miniprojectmanager.frontend/src/services/api.ts
+++ miniprojectmanager.frontend/src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5112/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5112/api",
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
